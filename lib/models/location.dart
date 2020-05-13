81eb8df7961c11bb71528fbbcb9cd373f878ad21
Nadia Opata <51159298+Nuerkie-Opt@users.noreply.github.com>
2020-05-13 07:41:10 +0000

    Add files via upload

---
+++ lib/models/location.dart
@@ -0,0 +1,20 @@
+import 'location_fact.dart';
+
+class Location{
+   String name;
+   String imagePath;
+    final List<LocationFact> facts;
+
+    Location(this.name, this.imagePath, this.facts);
+
+    static List<Location> fetchAll(){
+      return[
+        Location('Cartiliginous Fish', 'assests/images/fish.jpg',[
+          LocationFact('Summary', 
+          'Cartilaginous fish are fish that have a skeleton made of cartilage, rather than bone. All sharks, skates, and rays (e.g., the southern stingray) are cartilaginous fish' ),
+          LocationFact('Diversity',
+           'Chondrichthyan, (class Chondrichthyes), any member of the diverse group of cartilaginous fishes that includes the sharks, skates, rays, and chimaeras. ')
+        ])
+      ];
+    }
+}
